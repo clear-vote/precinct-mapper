2310d32a4cebd2fd88755f17fab94b09bf044f69
Anaya Pandit <anayap@uw.edu>
2023-12-28 23:43:49 -0800

    updated State obj

--- src/precinct_mapper/data/containers/state.py
+++ src/precinct_mapper/data/containers/state.py
@@ -1,5 +1,5 @@
 from shapely.geometry import MultiPolygon, Polygon, Point
-from . import Region, County, Precinct
+from . import Region, Precinct
 from geopandas import GeoDataFrame
 from typing import List
 
@@ -27,38 +27,38 @@ class State(Region):
                 "city"
                 ] + additional_columns,
                 geometry="precinct_geometry"
-            )
+        )
 
-    def get_county_names(self) -> List[str]:
-        """Returns the names of all counties in this state in ascending order"""
-        return sorted(list(self.counties["name"]))
+    # def get_county_names(self) -> List[str]:
+    #     """Returns the names of all counties in this state in ascending order"""
+    #     return sorted(list(self.counties["name"]))
     
-    def get_county_by_coordinate(self, coord: Point) -> County:
-        """Returns the County that contains the given coordinates.
+    # def get_county_by_coordinate(self, coord: Point) -> County:
+    #     """Returns the County that contains the given coordinates.
         
-        Args:
-            coord: coordinates to locate containing county of
+    #     Args:
+    #         coord: coordinates to locate containing county of
 
-        Raises:
-            LookupError: if no county in this state contains the given coordinates
-        """
-        for county in self.counties:
-            if county.contains(coord):
-                return county
-        raise LookupError(f"Could not find county containing coordinates: { coord }")
+    #     Raises:
+    #         LookupError: if no county in this state contains the given coordinates
+    #     """
+    #     for county in self.counties:
+    #         if county.contains(coord):
+    #             return county
+    #     raise LookupError(f"Could not find county containing coordinates: { coord }")
 
-    def get_county_by_name(self, county_name: str) -> County:
-        """Returns the County in this state with the given name"""
-        if county_name not in self.counties["name"]:
-            raise LookupError(f"Could not find county with name: { county_name }. Try one of {list(self.counties['name'])}")
+    # def get_county_by_name(self, county_name: str) -> County:
+    #     """Returns the County in this state with the given name"""
+    #     if county_name not in self.counties["name"]:
+    #         raise LookupError(f"Could not find county with name: { county_name }. Try one of {list(self.counties['name'])}")
         
-        return self.counties.loc[self.counties["name"] == county_name].iloc[0]
+    #     return self.counties.loc[self.counties["name"] == county_name].iloc[0]
 
-    def get_precinct(self, coord: Point) -> Precinct:
-        county = self.get_county()
-        return county.get_precinct(coord)
+    # def get_precinct(self, coord: Point) -> Precinct:
+    #     county = self.get_county()
+    #     return county.get_precinct(coord)
 
-    def add_county(self, county: County):
-        if county.name in self.counties["name"]:
-            raise ValueError(f"Given county ('{ county }') is already in this state")
-        self.counties[len(self.counties)] = [county.name, county, county.boundary]
+    # def add_county(self, county: County):
+    #     if county.name in self.counties["name"]:
+    #         raise ValueError(f"Given county ('{ county }') is already in this state")
+    #     self.counties[len(self.counties)] = [county.name, county, county.boundary]
