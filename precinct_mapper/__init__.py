1b0baf691df1fa625c84d23e91d6615210334a5c
Anaya Pandit <anayap@uw.edu>
2024-03-29 00:17:37 -0700

    added load_state() import to __init__.py

--- precinct_mapper/__init__.py
+++ precinct_mapper/__init__.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from precinct_mapper import mapper
 
+from precinct_mapper.mapper import load_state
+
 from precinct_mapper.data import (
     StateDataFetcher,
     WADataFetcher,
